af0575e6674a3e29e69855fe519118b3dd7a2579
partageonslaforet <git@partageonslaforet.be>
2023-10-29 16:51:49 +0100

    tets

--- README.md
+++
@@ -1,4 +0,0 @@
-# plf
-Partageons la foret
-Correctif pour l'utilisation de mobile.
-Signale que la version mobile sera disponible plus tard
--- desktop/uploadgpx.php
+++
@@ -1,4 +0,0 @@
-<?php
-$targetPath = "assets/datas/uploadgpx/".basename($_FILES["inputFile"]["name"]);
-move_uploaded_file($_FILES["inputFile"]["tmp_name"], $targetPath);
-?>
