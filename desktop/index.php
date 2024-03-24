c26b003dd3d91ccece2d61feb8abc86e6c36c5c1
lurkich <Christian.Lurkin@hotmail.com>
2024-03-24 16:38:32 +0100

    changed iindex.php

--- desktop/index.php
+++ desktop/index.php
@@ -96,7 +96,7 @@ if (isset($_POST['submit'])) {
     <body>
 
         <div id="title">
-            <div id="siteName">Soyez les bienvenus sur le site partageonslaforet.be </div>
+            <div id="siteName">Soyezez les bienvenus sur le site partageonslaforet.be </div>
             <div id="slogan">Découvrez docker dddles dates de chasse de la saison 2023/2024 en Wallonie</div>
         </div>
 
