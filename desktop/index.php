fde8d9482e90a43555bd3f8dba76910efbeebe9c
lurkich <Christian.Lurkin@hotmail.com>
2024-03-24 17:05:57 +0100

    ddddddd

--- desktop/index.php
+++ desktop/index.php
@@ -96,7 +96,7 @@ if (isset($_POST['submit'])) {
     <body>
 
         <div id="title">
-            <div id="siteName">Soyezezeee zles bienvenus sur le site partageonslaforet.be </div>
+            <div id="siteName">Soyezezeee ole ole zles bienvenus sur le site partageonslaforet.be </div>
             <div id="slogan">Découvrez docker dddles dates de chasse de la saison 2023/2024 en Wallonie</div>
         </div>
 
