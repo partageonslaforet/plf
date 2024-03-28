4644252de7fee64eb6d12afb86966f587f93a77a
lurkich <Christian.Lurkin@hotmail.com>
2024-03-28 13:27:15 +0100

    from jenkins to docker

--- desktop/index.php
+++ desktop/index.php
@@ -96,10 +96,8 @@ if (isset($_POST['submit'])) {
     <body>
 
         <div id="title">
-
             <div id="siteName">Soyez les bienvenus sur le site partageonslaforet.be </div>
-
-            <div id="slogan">Découvrez docker dddles dates de chasse de la saison 2023/2024 en Wallonie</div>
+            <div id="slogan">Découvrez les dates de chasse de la saison 2023/2024 en Wallonie</div>
         </div>
 
         <div id="popupInfo">
